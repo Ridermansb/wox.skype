dfd2e560eb7e2668351c8e0575f6049799881714
ridermansb <ridermansb@gmail.com>
2015-04-01 23:51:13 -0300

    Add unit tests

---
+++ Wox.Skype.Test/MainTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Wox.Plugin;
+using FluentAssertions;
+
+namespace Wox.Skype.Test
+{
+	[TestFixture]
+	public class MainTest
+	{
+		[Test]
+		public void EmptyContacts()
+		{
+			var main = new Main();
+			main.Init(new PluginInitContext());
+
+			var results = main.Query(new Query("skp a"));
+
+			results.Should().NotBeEmpty();
+		}
+
+
+	}
+}
