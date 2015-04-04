dcaa6a4163578232ea61992c7bdb773f2f69dd95
ridermansb <ridermansb@gmail.com>
2015-04-04 00:07:00 -0300

    Start conversation when select contact

--- Wox.Skype/Main.cs
+++ Wox.Skype/Main.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using Wox.Plugin;
 using System.Linq;
-using System.Diagnostics;
 
 namespace Wox.Skype
 {
@@ -27,25 +26,8 @@ namespace Wox.Skype
 					Title = "Open skype",
 					Action = e =>
 					{
-						try
-						{
-							_skype.Client.Start();
-						}
-						catch (System.Exception ex)
-						{
-							using (EventLog eventLog = new EventLog("Aplicativo"))
-							{
-								eventLog.Source = "Aplicativo";
-								eventLog.WriteEntry(ex.Message, EventLogEntryType.Information, 101, 1);
-							} 
-
-							var cs = "Wox.Skype";
-							if (!EventLog.SourceExists(cs))
-								EventLog.CreateEventSource(cs, "Aplicativo");
-
-							EventLog.WriteEntry(cs, ex.Message, EventLogEntryType.Error);
-						}
-						return false;
+						_skype.Client.Start();
+						return true;
 
 
 					}
@@ -54,59 +36,34 @@ namespace Wox.Skype
 			}
 
 
-			var str = query.ActionParameters[0]; //.ToLower();
+			var queryName = query.ActionParameters[0].ToLower();
 
-			//IEnumerable<SKYPE4COMLib.User> users = _skype.SearchForUsers(str).OfType<SKYPE4COMLib.User>(); // _skype.Friends.OfType<SKYPE4COMLib.User>();
-			foreach (User u in _skype.SearchForUsers(str))
-			{
-				try
+			_skype.Friends.OfType<SKYPE4COMLib.User>()
+				.Where(u => ((u.Aliases != null && !string.IsNullOrEmpty(u.Aliases) ? u.Aliases.ToLower().Contains(queryName) : false)
+						|| (u.DisplayName != null && !string.IsNullOrEmpty(u.DisplayName) ? u.DisplayName.ToLower().Contains(queryName) : false)
+						|| (u.FullName != null && !string.IsNullOrEmpty(u.FullName) ? u.FullName.ToLower().Contains(queryName) : false)
+						|| (u.PhoneHome != null && !string.IsNullOrEmpty(u.PhoneHome) ? u.PhoneHome.ToLower().Contains(queryName) : false)
+						|| (u.PhoneMobile != null && !string.IsNullOrEmpty(u.PhoneMobile) ? u.PhoneMobile.ToLower().Contains(queryName) : false)
+						|| (u.PhoneOffice != null && !string.IsNullOrEmpty(u.PhoneOffice) ? u.PhoneOffice.ToLower().Contains(queryName) : false))
+					)
+				.OrderBy(u => u.FullName)
+				.ToList()
+				.ForEach(u =>
 				{
+
 					results.Add(new Result()
-								{
-									Title = u.DisplayName,
-									SubTitle = u.MoodText,
-									IcoPath = "Images\\plugin.png",  //relative path to your plugin directory
-									Action = e =>
-									{
-										// after user select the item
+					{
+						Title = u.FullName,
+						SubTitle = u.MoodText,
+						IcoPath = "Images\\plugin.png",
+						Action = e =>
+						{
+							_skype.Client.OpenMessageDialog(u.Handle);
+							return true;
+						}
+					});
 
-										// return false to tell Wox don't hide query window, otherwise Wox will hide it automatically
-										return false;
-									}
-								});
-				}
-				catch { }
-			}
-
-			//users
-			//	.Where(u => u.OnlineStatus == TOnlineStatus.olsOnline
-			//		&& (u.Aliases != null && !string.IsNullOrEmpty(u.Aliases) ? u.Aliases.ToLower().Contains(str) : false
-			//			|| u.DisplayName != null && !string.IsNullOrEmpty(u.DisplayName) ? u.DisplayName.ToLower().Contains(str) : false
-			//			|| u.FullName != null && !string.IsNullOrEmpty(u.FullName) ? u.FullName.ToLower().Contains(str) : false
-			//			|| u.PhoneHome != null && !string.IsNullOrEmpty(u.PhoneHome) ? u.PhoneHome.ToLower().Contains(str) : false
-			//			|| u.PhoneMobile != null && !string.IsNullOrEmpty(u.PhoneMobile) ? u.PhoneMobile.ToLower().Contains(str) : false
-			//			|| u.PhoneOffice != null && !string.IsNullOrEmpty(u.PhoneOffice) ? u.PhoneOffice.ToLower().Contains(str) : false)
-			//		)
-			//	.OrderBy(u => u.FullName)
-			//	.ToList()
-			//	.ForEach(u =>
-			//	{
-
-			//		results.Add(new Result()
-			//		{
-			//			Title = u.DisplayName,
-			//			SubTitle = u.MoodText,
-			//			IcoPath = "Images\\plugin.png",  //relative path to your plugin directory
-			//			Action = e =>
-			//			{
-			//				// after user select the item
-
-			//				// return false to tell Wox don't hide query window, otherwise Wox will hide it automatically
-			//				return false;
-			//			}
-			//		});
-
-			//	});
+				});
 
 
 			return results;
