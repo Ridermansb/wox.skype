e52fccf1ce69f50f0fbc5cae02c06d49f64b3e5a
ridermansb <ridermansb@gmail.com>
2015-04-05 14:55:39 -0300

    Add env: and ls

--- upload-coverage-report.ps1
+++ upload-coverage-report.ps1
@@ -2,11 +2,13 @@ Write-Host -ForegroundColor DarkBlue "Upload coverage report ..."
 
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GITHUB_ACCESS_TOKEN):x-oauth-basic@github.com/Ridermansb/wox.skype.git"
 
+ls
+
 git clone -b gh-pages https://github.com/Ridermansb/wox.skype.git gh-pages
 
 cd gh-pages
 
-. ..\$reportPath  -reports:..\$env:coverResultFile -targetdir:coverage
+. ..\$env:reportPath  -reports:..\$env:coverResultFile -targetdir:coverage
 
 git add .
 
