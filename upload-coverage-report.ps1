5f5c3b6601e29ef36ad65431c20fef81b5868cb6
ridermansb <ridermansb@gmail.com>
2015-04-05 15:21:44 -0300

    Fix git erros in ps

--- upload-coverage-report.ps1
+++ upload-coverage-report.ps1
@@ -1,6 +1,6 @@
 Write-Host -ForegroundColor Cyan "Generate coverage report ..."
 
-git clone -b -n --depth=1 gh-pages "https://github.com/Ridermansb/wox.skype.git" gh-pages
+git clone -b -n --depth=1 gh-pages "https://github.com/Ridermansb/wox.skype.git" gh-pages 2>&1 | % { $_.ToString() }
 
 ls
 
@@ -12,8 +12,8 @@ cd gh-pages
 
 Write-Host -ForegroundColor Cyan "Upload coverage report ..."
 
-git add .
+git add . 2>&1 | % { $_.ToString() }
 
-git commit -am "Update coverage code report"
+git commit -am "Update coverage code report" 2>&1 | % { $_.ToString() }
 
-git push origin gh-pages
+git push origin gh-pages 2>&1 | % { $_.ToString() }
